5161e0e66d583ecadf20aec68148acf30de8e0ef
Devin Chen <d3vin.chen@gmail.com>
2017-04-06 18:58:36 +0800

    接口支持错误类型调试

--- api/mocks/outerentitytree_querymdtree.js
+++ api/mocks/outerentitytree_querymdtree.js
@@ -2,9 +2,11 @@ const debug = require('debug')('ssc:mocks');
 const low = require('lowdb');
 const utils = require('./utils');
 
-module.exports = {
-  post: post
-};
+/**
+ * - 0 正常
+ * - 1 查询失败
+ */
+const ERROR_TYPE = 0;
 
 function post(req, res) {
 
@@ -12,24 +14,38 @@ function post(req, res) {
   const mappingDefId = req.body.mappingdefid; // 1
 
   const resObj = {
-    __fake_server__: true,
-    "success": true,
-    "message": null
+    __fake_server__: true
   };
 
-  debug(`Open controller data file: outerentitytree_querymdtree.json`);
-  const db = low(`${__dirname}/ctrl_data/outerentitytree_querymdtree.json`);
-
-  // 为啥isEmpty返回的是Boolean对象?
-  if (!db.isEmpty().valueOf()) {
-    // const meta = db.get('__meta__').value(); // 获取meta信息
-    var data = db.get('data').value();
-    debug('data: %s', JSON.stringify(data));
-    resObj.data = data;
-  } else {
-    resObj.success = false;
-    resObj.message = '对应该类型的数据表JSON文件不存在，请检查api/mocks/ctrl_data/目录';
+  switch(ERROR_TYPE) {
+    default:
+    case 0:
+      debug(`Open controller data file: outerentitytree_querymdtree.json`);
+      const db = low(`${__dirname}/ctrl_data/outerentitytree_querymdtree.json`);
+      // 为啥isEmpty返回的是Boolean对象?
+      if (!db.isEmpty().valueOf()) {
+        // const meta = db.get('__meta__').value(); // 获取meta信息
+        var data = db.get('data').value();
+        debug('data: %s', JSON.stringify(data));
+        resObj.data = data;
+        resObj.success = true;
+        resObj.message = null;
+      } else {
+        resObj.success = false;
+        resObj.message = '对应该类型的数据表JSON文件不存在，请检查api/mocks/ctrl_data/目录';
+      }
+      res.json(resObj);
+      break;
+    case 1:
+      resObj.success = false;
+      resObj.message = '查询失败。null';
+      resObj.data = null;
+      resObj.code = 0;
+      res.json(resObj);
+      break;
   }
-
-  res.json(resObj);
 }
+
+module.exports = {
+  post: post
+};
